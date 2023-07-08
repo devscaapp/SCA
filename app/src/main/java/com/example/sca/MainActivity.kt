6241d700add01feeaf9468d1c954583a309352de
Vipin Pal <vipin4597@gmail.com>
2023-07-08 12:24:11 +0530

    name

--- app/src/main/java/com/example/sca/MainActivity.kt
+++ app/src/main/java/com/example/sca/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Shivam $name!",
+        text = "Hello Vipin $name!",
         modifier = modifier
     )
 }
