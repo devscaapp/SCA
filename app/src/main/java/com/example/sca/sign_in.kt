6777a7204e6f36bbd07be4ac7bd96e2ea608f156
devscaapp <dev.sca.app@gmail.com>
2023-08-16 19:02:26 +0530

    Sign in done

--- app/src/main/java/com/example/sca/sign_in.kt
+++ app/src/main/java/com/example/sca/sign_in.kt
@@ -2,12 +2,10 @@ package com.example.sca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class Register : AppCompatActivity() {
+class sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-         Button button = findViewById(R.id.button_id)
+        setContentView(R.layout.activity_sign_in)
     }
 }
