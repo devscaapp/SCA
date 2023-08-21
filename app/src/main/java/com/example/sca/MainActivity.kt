e5875fd4b4590e37b95d97378c8f1393ff1e93f8
devscaapp <dev.sca.app@gmail.com>
2023-08-21 15:39:41 +0530

    auth email and pass UI Update

--- app/src/main/java/com/example/sca/MainActivity.kt
+++ app/src/main/java/com/example/sca/MainActivity.kt
@@ -15,31 +15,7 @@ import com.example.sca.ui.theme.SCATheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SCATheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.home)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Vipin $name!",
-        modifier = modifier
-    )
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SCATheme {
-        Greeting("Android")
     }
 }
